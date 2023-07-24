d424b7ee3aed444fd766d49fdeb813d40848d963
ConstruisezUneAPI <guillaume.lacolley@gmail.com>
2023-07-24 21:39:48 +0200

    ajustement

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,6 +5,7 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.TOKEN);
     const userId = decodedToken.userId;
+    console.log(token, userId);
     if (req.body.userId && req.body.userId !== userId) {
       throw "Mot de passe invalide";
     } else {
